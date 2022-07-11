dc6a8d97b1a7863cdd106ee8b44ad24d92b7d9fd
qifei <qifei@zhongan.io>
2022-07-12 02:28:42 +0800

    update

--- web.src/src/App.vue
+++ web.src/src/App.vue
@@ -1,81 +1,96 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <a-layout>
+    <a-layout-header style="padding: 10px;">
+      <a-row>
+        <a-col :span="3">
+          <a-select size="large" type="primary" v-model="form.req.method">
+            <a-option v-for="item in methods" :value="item.value" :label="item.label" />
+          </a-select>
+        </a-col>
+        <a-col :span="18">
+          <a-input size="large" v-model="form.req.url" placeholder="URL" />
+        </a-col>
+        <a-col :span="3">
+          <a-button w size="large" type="primary" long @click="onSend">Send</a-button>
+        </a-col>
+      </a-row>
+    </a-layout-header>
+    <a-layout-content style="padding: 10px;">
+      <a-collapse :default-active-key="['1']" :bordered="false">
+        <a-collapse-item header="Request" key="1">
+          <a-tabs type="capsule">
+            <a-tab-pane key="1" title="Headers">
+              Content of Tab Panel 1
+            </a-tab-pane>
+            <a-tab-pane key="2" title="Body">
+              <a-textarea placeholder="Please enter something" allow-clear />
+            </a-tab-pane>
+          </a-tabs>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+        </a-collapse-item>
+        <a-collapse-item header="Response" key="3">
+          <a-textarea v-model="form.resp.body" :auto-size="{minRows: 5 }" />
+        </a-collapse-item>
+      </a-collapse>
+    </a-layout-content>
+  </a-layout>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
+
+
+<script>
+import aardio from 'aardio'
+
+export default {
+  data() {
+    return {
+      methods: [
+        { label: "GET", value: "GET" },
+        { label: "POST", value: "POST" },
+        { label: "PUT", value: "PUT" },
+        { label: "DELETE", value: "DELETE" },
+        { label: "HEAD", value: "HEAD" },
+        { label: "PATCH", value: "PATCH" },
+        { label: "OPTIONS", value: "OPTIONS" }
+      ],
+      form: {
+        req: {
+          url: "https://mcd-api.tst.gululu.com/",
+          method: "GET"
+        },
+        resp:{
+          body: ""
+        }
+      }
+    }
+  },
+  methods: {
+    onSend(){
+      aardio.httpRequest(this.form.req.url, "params", this.form.req.method, "headers")
+      .then(resp=>{
+        console.log("WEB >>", resp)
+      })
+      .catch (err => {
+        console.log("err >>", err)
+      })
+    }
+  },
+  mounted() {
+    console.log('xxxxxxxxxxxxx')
+  }
+}
+</script>
+
 
 <style>
 @import './assets/base.css';
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
+.arco-tabs-nav-tab {
+  justify-content: flex-start !important;
 }
 
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.arco-collapse-item-content {
+  background-color: var(--color-bg-1) !important;
 }
 </style>
--- web.src/vite.config.js
+++ web.src/vite.config.js
@@ -2,11 +2,18 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ArcoResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({ resolvers: [ArcoResolver({ sideEffect: true })] }),
+    AutoImport({ resolvers: [ArcoResolver()], }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
